c5ce94783500c94694ed959c758334beeaa2e8bd
Devin Mullin <devin.mullin90@gmail.com>
2021-10-13 18:13:45 -0500

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+    
+    def initialization(word)
+        @word=word
+    end 
+
+    def match(words_array)
+        words_array.select |word|
+        word.chars.sort == @word.chars.sort
+    end 
+end
+
+listen = Anagram.new
+listen.match(%w[enlists google inlets banana])
+
+
